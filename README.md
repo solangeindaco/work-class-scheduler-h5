ba9b6a1430f0ba8a182f005de65bff96f08ac5c7
SolangeI <solange.indaco@gmail.com>
2023-09-06 09:36:41 -0700

    Added comments to the script file

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 I deployed this repository to learn about JQuery and Day.js. The basic code for the website was provided by a Bootcamp course. I create a simple calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 I learn:  
-    - how to use Dayjs    
-    - how to  
+    - how to use Dayjs.    
+    - how to rename a repository.
 
 ## Installation
 
@@ -23,7 +23,7 @@ The following image shows the web application's appearance and functionality:
 
 To refactor this website, I used the following tutorials:  
     - Dayjs, JavaScript library: https://day.js.org/ 
-    - 
+    - Github: https://docs.github.com/en/repositories/creating-and-managing-repositories/renaming-a-repository    
 
 ## License
 
--- script.js
+++ script.js
@@ -1,62 +1,41 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-const timeClasses = ['past', 'present', 'future'];
-
-//Replace the time class in the time block
-function replaceClass(timeBlock, className){
-  if (!timeBlock.hasClass(className)){
-    timeClasses.forEach((timeClass) => {
-      if (timeBlock.hasClass(timeClass)){
-        timeBlock.removeClass(timeClass);
-      }
-    });
-    timeBlock.addClass(className);
-  }
-}
-//Convert the time block 12-clock-hour to a 24-clock hour
-function return24DayHour(twelveClockHour){
-  return ((9 <= twelveClockHour) && (twelveClockHour<= 12))? twelveClockHour : (twelveClockHour + 12);
-}
 
 $(function () {
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  //var advancedFormat = require('dayjs/plugin/advancedFormat');
-  //dayjs.extend(advancedFormat); 
+  // css time block classes
+  const timeClasses = ['past', 'present', 'future'];
 
-  //Use the Dayjs to return the current day 
-  //var advancedFormat = require('dayjs/plugin/advancedFormat');
-  //dayjs.extend(advancedFormat);
+  //Replace the time css class in the time block
+  function replaceClass(timeBlock, className){
+    if (!timeBlock.hasClass(className)){
+      timeClasses.forEach((timeClass) => {
+        if (timeBlock.hasClass(timeClass)){
+          timeBlock.removeClass(timeClass);
+        }
+      });
+      timeBlock.addClass(className);
+    }
+  }
 
+  //Convert the time block 12-clock-hour to a 24-clock hour
+  function return24DayHour(twelveClockHour){
+    return ((9 <= twelveClockHour) && (twelveClockHour<= 12))? twelveClockHour : (twelveClockHour + 12);
+  }
+
+  //Display the current date in the header of the page.
   var currentDate = dayjs();
   $('#currentDay').text(currentDate.format('dddd, MMMM D'));
+  //get the current hour in 24-hour time
   var currentHour = currentDate.hour()
 
   //Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour.
   var $timeBlocks = $('#container').children('.time-block');
   $.each($timeBlocks, function () {
+    // Get the time block hour from the id
     let timeBlockId = $(this).attr("id");
     let timeBlockHour = return24DayHour(parseInt(timeBlockId.split('-')[1]));
-  
+    
+    // Apply the past, present, or future class to each time
+    // block by comparing the time block hour to the current hour.
     if (timeBlockHour == currentHour){
       replaceClass($(this),'present');
     }else if (timeBlockHour < currentHour){ // Past
@@ -65,12 +44,12 @@ $(function () {
       replaceClass($(this),'future');
     }
 
-    //Display any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements
+   // Get any user input that was saved in localStorage and set
+   // the values of the corresponding textarea elements.
     let eventText = localStorage.getItem(timeBlockId);
     $(this).children('textarea').eq( 0 ).val(eventText);
 
-    //Save an event in the localstorage binding it with the timeblock id
+    //Save an event in the local storage binding it with the timeblock id
     $(this).on('click', function () {
       let event = $(this).children('textarea').eq( 0 ).val()
       localStorage.setItem(timeBlockId, event);
